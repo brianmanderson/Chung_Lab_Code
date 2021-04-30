492fa06f3f1dcce9ff806f0f0e58304ae9bdf9a1
markba122@gmail.com <bmanderson@mdanderson.org>
2021-04-30 12:15:38 -0500

    update to check if any of the files have a FrameOfReferenceUID and set it as such

--- Fixing_Multi_Inst_Data/AddFrameAndSeparate.py
+++ Fixing_Multi_Inst_Data/AddFrameAndSeparate.py
@@ -21,25 +21,34 @@ def add_frame_and_separate(path):
                 image_reader.Execute()
                 rows_cols = '{}_{}'.format(image_reader.GetMetaData("0028|0010"), image_reader.GetMetaData("0028|0011"))
                 if rows_cols not in folder_dict:
+                    folder_dict[rows_cols] = {'files': [], 'uid': None, 'needs_uid': []}
+                if image_reader.HasMetaDataKey("0020|0052"):
+                    if folder_dict[rows_cols]['uid'] is None:
+                        folder_dict[rows_cols]['uid'] = image_reader.GetMetaData("0020|0052")
+                    folder_dict[rows_cols]['needs_uid'].append(False)
+                else:
+                    folder_dict[rows_cols]['needs_uid'].append(True)
+                folder_dict[rows_cols]['files'].append(file)
+            for key in folder_dict:
+                base_uid = folder_dict[key]['uid']
+                if base_uid is None:
                     base_uid = '1.3.12.2.1107.5.1.4.95642.30000017092813491242400000003.{}'.format(
                         np.random.randint(9999))
-                    folder_dict[rows_cols] = {'files': [], 'uid': base_uid}
-                if not image_reader.HasMetaDataKey("0020|0052"):
+                file_list = np.asarray(folder_dict[key]['files'])[folder_dict[key]['needs_uid']]
+                for file in file_list:
                     ds = pydicom.read_file(file)
-                    ds.FrameOfReferenceUID = folder_dict[rows_cols]['uid']
+                    ds.FrameOfReferenceUID = base_uid
                     pydicom.write_file(filename=file, dataset=ds)
-                folder_dict[rows_cols]['files'].append(file)
-            if len(folder_dict) > 1:
-                for key in folder_dict:
+                if len(folder_dict) > 1:
                     os.makedirs(os.path.join(root, key))
                     for file in folder_dict[key]['files']:
                         file_name = os.path.split(file)[-1]
                         os.rename(file, os.path.join(root, key, file_name))
                     fid = open(os.path.join(root, key, 'Finished.txt'), 'w+')
                     fid.close()
-            else:
-                fid = open(finished, 'w+')
-                fid.close()
+                else:
+                    fid = open(finished, 'w+')
+                    fid.close()
     return None
 
 
